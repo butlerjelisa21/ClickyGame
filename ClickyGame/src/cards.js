8d00d1a6c14538c1905565024b0b965112ebe4ae
Jelisa Butler <jelisa.l.butler@gmail.com>
2019-11-13 20:44:40 -0600

    changes

---
+++ ClickyGame/src/cards.js
@@ -0,0 +1,73 @@
+
+[{
+    "id": 1,
+    "image": "https://assets.readitforward.com/wp-content/uploads/2017/02/rorshach.jpg"
+  },
+  {
+    "id": 2,
+    "image": "https://www.mediastorehouse.com/p/497/rorschach-inkblot-1921-first-inkblot-test-12392577.jpg"
+  },
+  {
+    "id": 3,
+
+    "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2gnEe3wdinfTnA671dRnX-QwCrW25ten9CJGW_HsBFxnm8Qwg"
+  },
+  {
+    "id": 4,
+    "image": "http://static.tvtropes.org/pmwiki/pub/images/InkblotTest.jpg"
+  },
+  {
+    "id": 5,
+    "image": "https://imgc.allpostersimages.com/img/print/posters/akova-rorschach-test_a-G-10358070-9664571.jpg"
+  },
+  {
+    "id": 6,
+    "image": "https://img00.deviantart.net/19ed/i/2007/055/6/1/rorschach_inkblob_bunny_test_1_by_ragdollstitches.jpg"
+
+  },
+  {
+    "id": 7,
+    "image": "https://i1.wp.com/www.psychexamreview.com/wp-content/uploads/2016/01/Rorschach_blot_02.jpg?zoom=2&resize=300%2C214"
+
+  },
+  {
+    "id": 8,
+    "image": "https://i0.wp.com/www.psychexamreview.com/wp-content/uploads/2016/01/800px-Rorschach_blot_03.jpg?zoom=2&resize=300%2C206"
+
+  },
+  {
+    "id": 9,
+    "image": "https://i1.wp.com/www.psychexamreview.com/wp-content/uploads/2016/01/Rorschach_blot_04.jpg?zoom=2&resize=300%2C203"
+
+  },
+  {
+    "id": 10,
+    "image": "https://i2.wp.com/www.psychexamreview.com/wp-content/uploads/2016/01/Rorschach_blot_05.jpg?zoom=2&resize=300%2C219"
+
+  },
+  {
+    "id": 11,
+    "image": "https://i2.wp.com/www.psychexamreview.com/wp-content/uploads/2016/01/Rorschach_blot_06.jpg?zoom=2&resize=300%2C222"
+
+  },
+  {
+    "id": 12,
+    "image": "https://i2.wp.com/www.psychexamreview.com/wp-content/uploads/2016/01/Rorschach_blot_07.jpg?zoom=2&resize=300%2C205"
+
+  },
+  {
+    "id": 13,
+    "image": "https://i2.wp.com/www.psychexamreview.com/wp-content/uploads/2016/01/689px-Rorschach_blot_08.jpg?zoom=2&resize=300%2C261"
+
+  },
+  {
+    "id": 14,
+    "image": "https://i2.wp.com/www.psychexamreview.com/wp-content/uploads/2016/01/647px-Rorschach_blot_09.jpg?zoom=2&resize=300%2C278"
+
+  },
+  {
+    "id": 15,
+    "image": "https://i0.wp.com/www.psychexamreview.com/wp-content/uploads/2016/01/751px-Rorschach_blot_10.jpg?zoom=2&resize=300%2C240"
+
+  }
+]
